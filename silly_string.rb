fd8c586ba10cf523599d22273917d542f14aab34
isabellademetz <isabella21136@gmail.com>
2018-07-09 22:23:56 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -45,7 +45,8 @@ puts proper_sentence
 sentence = "Astounding aardvarks, arguably an ancient animal, always ate apples and acorns and artichokes all around Athens, amazing!"
 
 # Swap all `A` for `O`, as well as all `a` for `o`. You need to chain `gsub`s.
-
+A = "O"
+O = "A"
 sentence = sentence # Add your gsub chain here
 
 puts sentence
